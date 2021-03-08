655487a1d7feeb99108f6d392ad0750c09e0c7ff
sanaz_javadi <sanazjavadi72@yahoo.com>
2021-03-08 16:52:39 +0330

    install packages

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import { store } from "./store";
-import {BrowserRouter} from 'react-router-dom'
+import { store, history } from "./store";
+import { Route, Switch } from "react-router-dom";
+import { ConnectedRouter } from "react-router-redux";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-   
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
 
   document.getElementById("root"),
--- src/store.js
+++ src/store.js
@@ -1,8 +1,26 @@
 import reducer from "./reducer";
 import { createStore, applyMiddleware } from "redux";
 import { localStorageMiddleWare } from "./middleware";
+import { routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
 
-export const store = createStore(
-  reducer,
-  applyMiddleware(localStorageMiddleWare),
-);
+export const history = createHistory();
+
+const myRouterMiddleware = routerMiddleware(history);
+
+const getMiddleware = () => {
+  if (process.env.NODE_ENV === "production") {
+    return applyMiddleware(
+      myRouterMiddleware,
+      localStorageMiddleWare
+    );
+  } else {
+    // Enable additional logging in non-production environments.
+    return applyMiddleware(
+      myRouterMiddleware,
+      localStorageMiddleWare
+    );
+  }
+};
+
+export const store = createStore(reducer, getMiddleware());
